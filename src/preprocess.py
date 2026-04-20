bd8f5482867987bbc9792471e029672a5545172d
Mihir Ganguli <mgangul@gmu.edu>
2026-04-20 14:02:22 -0400

    Days column transformer class, convert to positive days and convert DAYS_BIRTH to AGE_YEARS, drop DAYS_BIRTH after

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
+import pandas as pd
 
 class DaysEmployedFixer(BaseEstimator, TransformerMixin):
     '''Replace anomaly value 365243 (representing unemployment/retirement) with nan'''
@@ -10,7 +11,7 @@ class DaysEmployedFixer(BaseEstimator, TransformerMixin):
     def transform(self, X, y = None):
         df = X.copy()
         df['DAYS_EMPLOYED'] = df['DAYS_EMPLOYED'].replace(365243, np.nan)
-        
+
         return df
 
         
@@ -18,8 +19,27 @@ class DaysEmployedFixer(BaseEstimator, TransformerMixin):
 
 
 class DaysColumnsTransformer(BaseEstimator, TransformerMixin):
+    ''' 
+    Convert DAYS_BIRTH column to AGE_YEARS.
+    Convert rest of DAYS columns to positive values.
+    Drop original DAYS_BIRTH column.
+     '''
 
-    pass
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X, y = None):
+
+        df = X.copy()
+        #convert to days since birth age in years, easier to interpret
+        df['AGE_YEARS'] = (-df['DAYS_BIRTH'] / 365).astype(int)
+
+        days_cols = [col for col in df.columns if col.startswith('DAYS')]
+        for col in days_cols:
+            df[col] = -df[col]
+
+        df = df.drop(columns='DAYS_BIRTH', errors='ignore')
+        return df
 
 class FlagEngineer(BaseEstimator, TransformerMixin):
     pass

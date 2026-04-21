af8529636470e459b56336567f43eb1b1febbd79
Mihir Ganguli <mgangul@gmu.edu>
2026-04-21 00:19:20 -0400

    add baseline logistic regression training, validation, model saving, and results interpretation

--- src/train.py
+++ src/train.py
@@ -2,11 +2,57 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from src.preprocess import build_pipeline
+from src.preprocess import build_pipeline, DropColumns, DaysEmployedFixer, DaysColumnsTransformer, FlagEngineer, RatioEngineer, RareCategoryGrouper
+from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, roc_auc_score
 import joblib
 
+df = pd.read_csv("data/raw/application_train.csv")
+
+X = df.drop(columns = 'TARGET')
+y= df['TARGET']
+
+X_train, X_temp, y_train, y_temp = train_test_split(
+    X, y, test_size=0.3, random_state=23, stratify=y
+)
+
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=23, stratify=y_temp)
+
+early_pipeline = Pipeline([
+    ('drop_columns', DropColumns()),
+    ('days_employed_fixer', DaysEmployedFixer()),
+    ('flag_engineer', FlagEngineer()),
+    ('days_transformer', DaysColumnsTransformer()),
+    ('ratio_engineer', RatioEngineer()),
+    ('rare_category_grouper', RareCategoryGrouper(cols=['OCCUPATION_TYPE', 'ORGANIZATION_TYPE'])),
+])
+
+X_train_transformed = early_pipeline.fit_transform(X_train)
+
+# now derive correct column lists
+num_cols = list(X_train_transformed.select_dtypes(include='number').columns)
+cat_cols = list(X_train_transformed.select_dtypes(include='object').columns)
+
+pipeline = build_pipeline(num_cols, cat_cols, LogisticRegression(class_weight='balanced', max_iter=1000), scale_features=True)
 
 
+pipeline.fit(X_train, y_train)
+
+
+y_val_pred = pipeline.predict(X_val)
+
+y_val_proba = pipeline.predict_proba(X_val)[:,1]
+print(f"ROC-AUC: {roc_auc_score(y_val, y_val_proba):.4f}")
+
+
+print(classification_report(y_val, y_val_pred))
+
+
+print("reached save section")
+joblib.dump(pipeline, 'models/baseline_logistic_regression.joblib')
+print("pipeline saved")
+joblib.dump(X_test, 'data/processed/X_test.joblib')
+joblib.dump(y_test, 'data/processed/y_test.joblib')
+print("test splits saved")
